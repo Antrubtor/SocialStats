81768720c57a10a980b834ea2711f5650bd83eb9
Antrubtor <antrubtor@gmail.com>
2025-07-17 01:50:46 +0200

    fix(snap): added more stats

--- src/snapchat.py
+++ src/snapchat.py
@@ -10,7 +10,7 @@ class SnapChat(SocialNetwork):
         selected.execute()
 
     def messages_process(self):
-        try:
+        # try:
             with zipfile.ZipFile(self.path, mode="r") as package:
                 with package.open("json/account.json", mode="r") as account:
                     sections = json.load(account)
@@ -21,31 +21,47 @@ class SnapChat(SocialNetwork):
                 with package.open("json/chat_history.json", mode="r") as msg:
                     sections = json.load(msg)
                     messages_per_day = {} # date : { name : (nb_you, nb_oth), name : (nb_you, nb_oth) }
-
-                    nb_msg, character = 0, 0
+                    hour_distribution = [0] * 24
+                    per_contact_stats = {}  # contact: (char_you, char_oth, msg_you, msg_oth, voice_msg_you, voice_msg_oth)
+                    total_msg, total_chr = 0, 0
                     for contact, messages in sections.items():
-                        nb_character_contact, nb_msg_contact = 0, 0
                         for message in messages:
-                            nb_msg_contact += 1
-                            if message["Media Type"] == "TEXT" and message["Content"] is not None:
-                                nb_character_contact += len(message["Content"])
-                            date = datetime.strptime(message["Created"], "%Y-%m-%d %H:%M:%S %Z").strftime("%Y/%m/%d")
-                            if date not in messages_per_day:
-                                messages_per_day[date] = {}
-                            nb_you, nb_oth = messages_per_day[date].get(contact, (0, 0))
-                            if message["From"] != pseudo:
+                            is_you = message["IsSender"]
+                            dt = datetime.fromtimestamp(int(message["Created(microseconds)"]) // 1000)
+                            date_str = dt.strftime("%m/%d/%Y")
+                            hour = dt.hour
+                            if is_you:
+                                hour_distribution[hour] += 1
+                            if date_str not in messages_per_day:
+                                messages_per_day[date_str] = {}
+
+                            nb_you, nb_oth = messages_per_day[date_str].get(contact, (0, 0))
+                            if is_you:
                                 nb_oth += 1
                             else:
                                 nb_you += 1
-                            messages_per_day[date][contact] = (nb_you, nb_oth)
-                        print(f"{contact} contains {nb_msg_contact} messages with {nb_character_contact} characters")
-                        character += nb_character_contact
-                        nb_msg += nb_msg_contact
+                            messages_per_day[date_str][contact] = (nb_you, nb_oth)
+                            content = message["Content"]
+                            nb_chars = len(content) if message["Media Type"] == "TEXT" and content else 0
+                            char_you, char_oth, msg_you, msg_oth = per_contact_stats.get(contact, (0, 0, 0, 0))
+                            if is_you:
+                                char_you += nb_chars
+                                msg_you += 1
+                            else:
+                                char_oth += nb_chars
+                                msg_oth += 1
+                            per_contact_stats[contact] = (char_you, char_oth, msg_you, msg_oth)
 
-                    print(f"\nLoaded {nb_msg} messages in total with {character} characters")
-                    return messages_per_day
-        except Exception as e:
-            print(e)
+                            total_chr += nb_chars
+                            total_msg += 1
+
+                    print(f"\nLoaded {total_msg} messages in total with {total_chr} characters")
+                    print(messages_per_day)
+                    # print(per_contact_stats)
+                    # print(hour_distribution)
+                    return messages_per_day, per_contact_stats, hour_distribution
+        # except Exception as e:
+        #     print(e)
 
 
     def medias_process(self):
